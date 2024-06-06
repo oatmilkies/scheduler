469a03f846d9012e606269c94a3a255c705e26d4
Leilani <pquaby@gmail.com>
2024-06-06 13:39:44 -0400

    Added tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+
 
 import Application from "components/Application";
 
@@ -8,8 +9,13 @@ afterEach(cleanup);
 
 describe("Appointment", () => {
 
-  it("renders without crashing", () => {
-    render(<Application />);
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { queryByText, findByText } = render(<Application />);
+
+    return findByText("Monday").then(() => {
+      fireEvent.click(queryByText("Tuesday"));
+      expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 
 });
