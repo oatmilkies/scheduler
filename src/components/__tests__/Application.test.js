1dcfd8fe46e004942340284b97263c2f304841d4
Leilani <pquaby@gmail.com>
2024-06-06 13:46:42 -0400

    Change to async

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -8,14 +8,16 @@ import Application from "components/Application";
 afterEach(cleanup);
 
 describe("Appointment", () => {
-
-  it("defaults to Monday and changes the schedule when a new day is selected", () => {
-    const { queryByText, findByText } = render(<Application />);
-
-    return findByText("Monday").then(() => {
-      fireEvent.click(queryByText("Tuesday"));
-      expect(queryByText("Leopold Silvers")).toBeInTheDocument();
-    });
+  //asynchronous test using asyn/await
+  //async function has been defined as one using the async keyword
+  //the promise chain can be hidden by using the await keyword
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { queryByText, getByText,findByText } = render(<Application />);
+  
+    await findByText("Monday");
+  
+    fireEvent.click(getByText("Tuesday"));
+    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
   });
 
 });
