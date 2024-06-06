fc5b6883c6cbdd6f822ea8498ca06b35982ad386
Leilani <pquaby@gmail.com>
2024-06-06 13:30:55 -0400

    Changed tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,10 +3,20 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
+  //Update the mode - helps users go from one component view to another
   function transition(newMode, replace = false) {
-    setHistory(prev => [...prev, newMode]);
+    setHistory(prev => {
+      const history = [...prev];
+      if (replace && history.length > 0) {
+        history[history.length - 1] = newMode; // Replace the last element
+      } else {
+        history.push(newMode); // Add new mode to history
+      }
+      return history;
+    });
   }
 
+  //Return to previous mode - if user doesn't want to book appt anymore
   function back() {
     if (history.length > 1) {
       setHistory(prev => [...prev.slice(0, prev.length - 1)]);
