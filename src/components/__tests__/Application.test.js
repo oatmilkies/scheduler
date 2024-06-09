31a7e661630479d87bcfe462b60a0459a8a8efef
Leilani <pquaby@gmail.com>
2024-06-09 09:57:28 -0400

    Added to Application test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,7 +6,8 @@ import {
   findByText,
   getAllByTestId,
   getByAltText,
-  getByPlaceholderText
+  getByPlaceholderText,
+  queryByText
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -31,22 +32,28 @@ describe("Appointment", () => {
 describe("Application", () => {
   it("loads data, books an interview and reduces the spots remaining for the first day by 1",
     async () => {
-      const { container } = render(<Application />);
+      const { container, debug } = render(<Application />);
 
       await findByText(container, "Archie Cohen");
-
+    
       const appointments = getAllByTestId(container, "appointment");
       const appointment = appointments[0];
-
+    
       fireEvent.click(getByAltText(appointment, "Add"));
-
+    
       fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
         target: { value: "Lydia Miller-Jones" },
       });
+    
       fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-
       fireEvent.click(getByText(appointment, "Save"));
-
-      console.log(prettyDOM(appointment));
+    
+      expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    
+      await findByText(appointment, "Lydia Miller-Jones");
+    
+      const day = getAllByTestId(container, "day").find((day) => queryByText(day, "Monday"));
+    
+      expect(getByText(day, "no spots remaining")).toBeInTheDocument();
     });
 });
