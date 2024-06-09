13c58fc871427ea067e4e2cfeb580316c87d29ca
Leilani <pquaby@gmail.com>
2024-06-09 10:08:17 -0400

    Add comments

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -32,28 +32,36 @@ describe("Appointment", () => {
 describe("Application", () => {
   it("loads data, books an interview and reduces the spots remaining for the first day by 1",
     async () => {
+      // 1. Render the Application.
       const { container, debug } = render(<Application />);
 
+      // 2. Wait until the text "Archie Cohen" is displayed.
       await findByText(container, "Archie Cohen");
-    
+
+      // 3. Click the "Add" button on the first empty appointment.
       const appointments = getAllByTestId(container, "appointment");
       const appointment = appointments[0];
     
       fireEvent.click(getByAltText(appointment, "Add"));
     
+      // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
       fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
         target: { value: "Lydia Miller-Jones" },
       });
     
+      // 5. Click the first interviewer in the list.
       fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+      // 6. Click the "Save" button on that same appointment.
       fireEvent.click(getByText(appointment, "Save"));
     
+      // 7. Check that the element with the text "Saving" is displayed.
       expect(getByText(appointment, "Saving")).toBeInTheDocument();
     
+      // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
       await findByText(appointment, "Lydia Miller-Jones");
-    
-      const day = getAllByTestId(container, "day").find((day) => queryByText(day, "Monday"));
-    
+
+    // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
+      const day = getAllByTestId(container, "day").find((day) => queryByText(day, "Monday"));    
       expect(getByText(day, "no spots remaining")).toBeInTheDocument();
     });
 });
